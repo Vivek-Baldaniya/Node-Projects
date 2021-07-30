3e75d929255bb867f205a4fa96f0ee873b446dad
Vivek <vivekbaldaniya@gmail.com>
2021-07-30 22:13:53 +0530

    add new files

--- Node-Express/app.js
+++ Node-Express/app.js
@@ -3,14 +3,21 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const adminData = require('./Routes/admin');
-const shopRoutes = require('./Routes/shop');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+// app.engine('hbs', expressHbs({
+//     extname: 'hbs',
+//     defaultLayout: 'main-layout',
+//     layoutsDir: 'views/layouts'
+// }));
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+const adminData = require('./Routes/admin');
+const shopRoutes = require('./Routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
